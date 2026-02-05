e751f9cad1635a53fe1954875201434615006942
Alxmr36 <Alexmr3639@gmail.com>
2026-02-05 14:00:08 -0600

    Create This is second.py

---
+++ This is second.py
@@ -0,0 +1,27 @@
+def fillBox():
+    items = 60
+    box = 0
+
+    if items >= 60:
+        print("Box is full")
+        box += 1
+        print("you now have", box, "box")
+    else:
+        print("Continue to fill the box")
+        print("You now have", items, "items in the box")
+
+print(fillBox())
+
+def createPallet():
+    boxes = 5
+    pallet = 0
+
+    if boxes >= 10:
+        print("Pallet is full")
+        pallet += 1
+        print("you now have", pallet, "pallet")
+    else:
+        print("Continue to fill the pallet")
+        print("You now have", boxes, "boxes on the pallet")
+
+print (createPallet())        
